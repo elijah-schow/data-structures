126da4bd0a36c22e8cf9fee8947a8c78f8ec9a45
elijah-schow <elijah.schow@gmail.com>
2017-02-01 11:38:28 -0600

    Implement functional Queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    someInstance[size] = value;
   };
 
   someInstance.pop = function() {
+    var result = someInstance[size];
+    delete someInstance[size];
+    size = Math.max(size - 1, 0);
+    return result;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
