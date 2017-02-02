ee7a3ae6244c3880339389e34a48d4a3f4c9f6f5
elijah-schow <elijah.schow@gmail.com>
2017-02-01 18:47:00 -0600

    Implement - pseudoclassical Stack function

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeProp = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.sizeProp++;
+  this.storage[this.sizeProp] = value;
+}
 
+Stack.prototype.pop = function() {
+  var result = this.storage[this.sizeProp];
+  delete this.storage[this.sizeProp];
+  if (this.sizeProp > 0) {
+    this.sizeProp--;
+  }
+  return result;
+}
+
+Stack.prototype.size = function() {
+  return this.sizeProp;
+}
