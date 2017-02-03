41c634c855e834c1e5b3f07664099dc543a679ed
elijah-schow <elijah.schow@gmail.com>
2017-02-02 19:49:51 -0600

    Implement - hashTable
    passes: 29 failures: 4

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index,v);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    bucket.push([k, v]);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  var result;
+  if (bucket) {
+    if (bucket.length === 1) {
+      result = bucket[0][1];
+    } else {
+      _.each(bucket, function(item) {
+        if (item[0] === k) {
+          result = item[1];
+        }
+      });
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
@@ -25,5 +43,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
