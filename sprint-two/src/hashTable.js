eed6d623071ddd888a5e4a189b2740b440a0286b
elijah-schow <elijah.schow@gmail.com>
2017-02-03 19:49:37 -0600

    Resize the hashTable if it is <25% or >75% full

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,17 +1,21 @@
-
-
 var HashTable = function() {
   this._limit = 8;
+  this._filled = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
+  //Handle Collisions
   if (bucket) {
     bucket.push([k, v]);
   } else {
     this._storage.set(index, [[k, v]]);
+  }
+  this._filled++;
+  if(this.percentFilled() > 75){
+    this.resize(this._limit * 2);
   }
 };
 
@@ -36,8 +40,33 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(index, undefined);
+  this._filled--;
+  if(this.percentFilled() < 25){
+    console.log('limit: ', this._limit);
+    console.log('% filled: ', this.percentFilled());
+    this.resize(this._limit / 2);
+  }
 };
 
+HashTable.prototype.resize = function(limit) {
+  var resized;
+  limit = Math.round(limit);
+  resized = LimitedArray(limit);
+  this._storage.each(function(array){
+    _.each(array, function(tuple){
+      var index = getIndexBelowMaxForKey(tuple[0], limit);
+      resized.set(index, resized[index] || []);
+      resized.get(index).push([tuple[0], tuple[1]]);
+    });
+  });
+  this._storage = resized;
+  this._limit = limit;
+  console.log('resized limit: ', this._limit);
+};
+
+HashTable.prototype.percentFilled = function(){
+  return this._filled / this._limit * 100;
+};
 
 
 /*
